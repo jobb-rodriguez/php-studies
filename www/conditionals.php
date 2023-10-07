9b9859b1a8cac198ef9870b4b0b8d3a3309b9423
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-10-07 12:41:55 +0800

    Add: Conditionals

---
+++ www/conditionals.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conditionals</title>
+</head>
+<body>
+    <!-- if(){}elseif(){}else{} ; && || !; >, >=, <, <=, ==, != -->
+    <!-- switch(var){case val: expression;break;default:expression;break;} -->
+    <!-- while(condition){expression;update;} -->
+    <!-- do{expression;update;}while(condition); -->
+    <!-- for(assign;condition;update){expression;} -->
+</body>
+</html>
