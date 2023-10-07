71f7b47624ff7a56192e2b82a54d22f401b061fe
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-10-07 12:49:47 +0800

    Add: Including PHP

--- www/including.php
+++ www/including.php
@@ -7,6 +7,15 @@
 </head>
 <body>
     <?php include "./html/header.html" ?>
+    <?php 
+        $title = "Harry Potter";
+        $author = "J.K. Rowling";
+        $wordCount = 50348;
+
+        include "php-templates/article-header.php";
+        include "php-tools/computations.php";
+    ?>
+    <p>The square of 27 is <?php echo square(27) ?>.</p>
     <?php include "./html/footer.html" ?>
 </body>
 </html>
---
+++ www/php-tools/computations.php
@@ -0,0 +1,5 @@
+<?php
+    function square($base) {
+        return $base * $base;
+    }
+?>
